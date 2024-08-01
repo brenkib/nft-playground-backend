5e8fc89556189574bacef62f77444a0627dcd7a5
Bren Brenkib <kennybush96@gmail.com>
2024-08-01 10:40:55 +0200

    Updated NFT contract

--- contracts/BrenkibNFTWithERC20Payment.sol
+++ contracts/BrenkibNFTWithERC20Payment.sol
@@ -2,49 +2,76 @@
 // Compatible with OpenZeppelin Contracts ^5.0.0
 pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {ERC721Enumerable} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import {ERC721URIStorage} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 contract BrenkibNFT_2 is ERC721Enumerable, ERC721URIStorage, Ownable {
+    using SafeERC20 for IERC20;
+
+    /**
+    * @dev Indicates a failure with the `spender` to be approved. Used in approvals.
+     * @param spender Address that initiated payment that failed.
+     */
+    error ERC20TokenPaymentFailed(address spender);
+
+    /**
+    * @dev Indicates a failure with the `withdrawPayments` method. Used by owner of this Contract.
+     * @param owner Address that initiated withdrawal that failed.
+     */
+    error ERC20TokenWithdrawalFailed(address owner);
+
+    /**
+     * @dev Unique Token Id counter for NFTs
+     */
     uint256 private _nextTokenId;
 
-    ERC20 public acceptedToken;
-    uint256 public mintPrice;
+    /**
+     * @dev Unique Token Id counter for NFTs
+     */
+    IERC20 private _acceptedToken;
+    uint256 private _mintPrice;
 
-    constructor(address _initialOwner, address _acceptedToken, uint256 _mintPrice) ERC721("BrenkibNFT", "BRN") Ownable(_initialOwner) {
-        acceptedToken = ERC20(_acceptedToken);
-        mintPrice = _mintPrice * 10 ** acceptedToken.decimals();
+    constructor(address initialOwner, address acceptedToken, uint256 mintPrice) ERC721("BrenkibNFT", "BRN") Ownable(initialOwner) {
+        _acceptedToken = IERC20(acceptedToken);
+        _mintPrice = mintPrice * 10 ** 18;
     }
 
     function safeMint(address to, string memory uri) public {
-        require(acceptedToken.transferFrom(msg.sender, address(this), mintPrice), "Payment failed");
+        //require(acceptedToken.transferFrom(msg.sender, address(this), mintPrice), "Payment failed");
+        if(_acceptedToken.transferFrom(msg.sender, address(this), _mintPrice) == false) {
+            revert ERC20TokenPaymentFailed(msg.sender);
+        }
         uint256 tokenId = _nextTokenId++;
         _safeMint(to, tokenId);
         _setTokenURI(tokenId, uri);
     }
 
     function setMintPrice(uint256 newPrice) external onlyOwner {
-        mintPrice = newPrice;
+        _mintPrice = newPrice;
     }
 
     function setAcceptedToken(address newToken) external onlyOwner {
-        acceptedToken = ERC20(newToken);
+        _acceptedToken = IERC20(newToken);
     }
 
     function getMintPrice() external view returns(uint256) {
-        return mintPrice;
+        return _mintPrice;
     }
 
     function getAcceptedToken() external view returns(address) {
-        return address(acceptedToken);
+        return address(_acceptedToken);
     }
 
     function withdrawPayments() external onlyOwner {
-        uint256 balance = acceptedToken.balanceOf(address(this));
-        require(acceptedToken.transfer(owner(), balance), "Withdrawal failed");
+        uint256 balance = _acceptedToken.balanceOf(address(this));
+        //require(acceptedToken.transfer(owner(), balance), "Withdrawal failed");
+        if(_acceptedToken.transfer(owner(), balance) == false) {
+            revert ERC20TokenWithdrawalFailed(owner());
+        }
     }
 
     // The following functions are overrides required by Solidity.
